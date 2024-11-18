4a51573aff4023c0d342c7b848f4e099a65de6f3
Thakur-Aman-Singh <singhel.aman16@gmail.com>
2024-11-18 19:26:44 +0530

    Create FirstDemo.java
    this is initial commit

---
+++ FirstDemo.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class FirstDemo{
+
+
+public static void main(String[] args) {
+    
+    System.out.println("Hello Form CI/CD");
+
+    System.out.println("This is current Time --->"+new Date());
+}
+
+}
